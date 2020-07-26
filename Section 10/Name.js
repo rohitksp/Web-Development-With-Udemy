8868f942bbbd328d4067665423a606ba8b75fad4
Chandan <kchandan@thoughtworks.com>
2020-07-26 21:16:45 +0530

    Demonstrating pull request

--- Section 10/Name.js
+++ Section 10/Name.js
@@ -1,5 +1,5 @@
 var username = prompt("What is your name?");
 var lastname = prompt("What is last name?");
 var age = prompt("What is your age");
-var hobbie =prompt("Please tell me your any hobbie");
-alert("Your full name is "+ "= "+ username +" "+lastname + "\nYour age is " + "= "+age +"\nHere is your hobbie "+ "= "+hobbie);
+var hobby = prompt("Please tell me your any hobby");
+alert("Your full name is " + "= " + username + " " +lastname + "\nYour age is " + "= " + age + "\nHere is your hobby " + "= " + hobbie);
