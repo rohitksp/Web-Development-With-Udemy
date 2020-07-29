cb5a48cd8a95caaf1645abe4556bc297cef60724
Rohit7065 <rohitgta7@gmail.com>
2020-07-29 07:20:37 +0200

    Add

---
+++ Section 11/For Loop.js
@@ -0,0 +1,4 @@
+var str = "Hello";
+for(var i = 0; i < str.length; i++) {
+    console.log(str[i]);
+}
---
+++ Section 11/Loop Problem Set.js
@@ -0,0 +1,24 @@
+//for(var i = -10; i < 20; i++){
+//console.log(i)
+//}
+
+for(var i = 10; i <= 40; i+=1){
+    if(i% 2===0){
+    console.log(i);
+}
+}
+for (var i = 10; i < 40; i+=2) {
+    console.log(i);   
+}
+
+for(var i = 300; i <=333; i++) {
+if(i% 2 !==0){
+    console.log(i);
+}
+}
+
+for (var i = 5; i <=50;i++) {
+    if(i % 5 ===0 && i % 3 ===0){
+        console.log(i);
+    }
+    }
