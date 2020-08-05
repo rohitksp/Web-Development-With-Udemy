023a84fa24cce171e87665ab757b2de9a93a3f99
Rohit7065 <rohitgta7@gmail.com>
2020-08-05 07:33:24 +0200

    add more things

--- Section 11 -  Javascript Basics Control Flow/Annoy Ometic.js
+++ Section 11 -  Javascript Basics Control Flow/Annoy Ometic.js
@@ -1,12 +1,12 @@
 // var tell = prompt("Your name is Rohit?");
 // while(tell.indexOf("Yes") === -1) {
 //    var tell = prompt("Your name is Rohit?");
-//}
+// }
 //  alert("OKAY THANKS!!");
 
-var tell = prompt("Your name is Rohit?");
-while (tell !== "Yes" && tell !== "Ya") {
-    var tell = prompt("Your name is Rohit?");
-    console.log(tell);
+var tell = window.prompt('Your name is Rohit?')
+while (tell !== 'Yes') {
+  tell = window.prompt('Your name is Rohit?')
+  console.log(tell)
 }
-alert("OKAY THANKS!!");
+window.alert('OKAY THANKS!!')
