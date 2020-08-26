4ecd1f31dae8c0980c7edfaa8d54398f6b09d1b8
Rohit7065 <rohitgta7@gmail.com>
2020-08-26 08:49:59 +0200

    commit

--- Section 13 - Javascript Arrays/todolist.js
+++ Section 13 - Javascript Arrays/todolist.js
@@ -1,15 +1,33 @@
 window.setTimeout(function () {
-  var todo = ["Hello How are you, What's going on now!"]
-  var type = window.prompt('Hello welcome to this game')
-  while (todo !== 'quit') {
-    if (type === 'list') {
-      console.log(todo)
-    } else if (type === 'new') {
-      var add = window.prompt('Enter new todo')
-      todo.push(add)
-      console.log(todo)
+  var todos = []
+  var input = window.prompt('Select any of the option that you are watching on screen.')
+  while (input !== 'quit') {
+    if (input === 'list') {
+      listTodo()
+    } else if (input === 'new') {
+      newTodo()
+    } else if (input === 'delete') {
+      deleteTodo()
     }
-    todo = window.prompt('What would you like to do?')
+    input = window.prompt('Select any of the option that you are watching on screen.')
   }
-  console.log('Ok you are quit this app')
+  console.log('THANKS FOR USING ME HOPE YOU LIKE THIS.')
+
+  function listTodo () {
+    console.log('**********')
+    todos.forEach(function (todo, i) {
+      console.log(i + ': ' + todo)
+    })
+    console.log('**********')
+  }
+  function newTodo () {
+    var add = window.prompt('Please add your new todo here.')
+    todos.push(add)
+    console.log('YOUR TODO HAS SUCCESSFULLY ADDED')
+  }
+  function deleteTodo () {
+    var index = window.prompt('Which todo do you want to delete?')
+    todos.splice(index, 1)
+    console.log('YOUR TODO SUCCESSFULLY REMOVED.')
+  }
 }, 500)
